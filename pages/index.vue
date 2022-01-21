457470f0203e06e5d46e965c5c3409b8c2b5762d
paulascotto <62989555+paulascotto@users.noreply.github.com>
2022-01-21 13:57:50 +0100

    first-project-commit

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home-page">
+    <section class="intro">
+      <h1>Get the latest tech news!</h1>
+    </section>
+    <section class="featured-posts">
+      <PostPreview 
+        id='1'
+        thumbnail= "https://cdn.techinasia.com/wp-content/uploads/2017/01/code-1839406_1280-750x500.jpg"
+        title="Hello there!"
+        previewText="This is my first post!"/>
+      <PostPreview 
+        id='1'
+        thumbnail= "https://cdn.techinasia.com/wp-content/uploads/2017/01/code-1839406_1280-750x500.jpg"
+        title="Hello there for the second time!"
+        previewText="This is my Second post!"/>
+      <PostPreview 
+        id='1'
+        thumbnail= "https://cdn.techinasia.com/wp-content/uploads/2017/01/code-1839406_1280-750x500.jpg"
+        title="Hello there for the third time!"
+        previewText="This is my Third post!"/>        
+    </section>
+  </div>
+</template>
+
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+
+export default{
+  components: {
+    PostPreview: PostPreview
+  }
+}
+</script>
+
+
